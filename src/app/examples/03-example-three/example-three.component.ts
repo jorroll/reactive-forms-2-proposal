327bc41041e999fdccd656fbf3431abdef58aefc
John Carroll <john.carroll.p@gmail.com>
2019-08-05 14:06:29 -0700

    simplify example

--- src/app/examples/03-example-three/example-three.component.ts
+++ src/app/examples/03-example-three/example-three.component.ts
@@ -2,81 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { ValidatorFn, FormControl } from '../../reactive-forms-two';
 import { filter, map } from 'rxjs/operators';
 
-@Component({
-  selector: 'app-example-three',
-  templateUrl: './example-three.component.html',
-  styleUrls: ['./example-three.component.scss'],
-})
-export class ExampleThreeComponent implements OnInit {
-  inputControl: FormControl<string>;
-  dateControl: FormControl<Date | null>;
+// regex from https://www.regextester.com/96683
+const dateRegex = /^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))$/;
 
-  constructor() {}
-
-  ngOnInit() {
-    // regex from https://www.regextester.com/96683
-    const dateRegex = /^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))$/;
-
-    const dateValidatorFn: ValidatorFn = control => {
-      if (dateRegex.test(control.value)) {
-        return null;
-      }
-
-      return {
-        invalidDate: 'Invalid date format! Try YYYY-MM-DD',
-      };
-    };
-
-    this.inputControl = new FormControl({
-      value: '',
-      validators: dateValidatorFn,
-    });
-
-    this.dateControl = new FormControl<Date | null>({ value: null });
-
-    this.inputControl.changes
-      .pipe(
-        map(state => {
-          switch (state.type) {
-            case 'setValue':
-            case 'patchValue':
-              return {
-                ...state,
-                value: dateRegex.test(state.value)
-                  ? stringToDate(state.value)
-                  : null,
-              };
-            default:
-              return state;
-          }
-        }),
-      )
-      .subscribe(this.dateControl.source);
-
-    this.dateControl.changes
-      .pipe(
-        map(state => {
-          // changes originating from the inputControl should be
-          // passed back unmodified
-          if (state.sources[0] === this.inputControl.id) {
-            return state;
-          }
-
-          switch (state.type) {
-            case 'setValue':
-            case 'patchValue':
-              return {
-                ...state,
-                value: dateToString(state.value),
-              };
-            default:
-              return state;
-          }
-        }),
-      )
-      .subscribe(this.inputControl.source);
+const dateValidatorFn: ValidatorFn = control => {
+  if (dateRegex.test(control.value)) {
+    return null;
   }
-}
+
+  return {
+    invalidDate: 'Invalid date format! Try YYYY-MM-DD',
+  };
+};
 
 function dateToString(date: Date | null) {
   if (!date) return '';
@@ -93,6 +30,8 @@ function padString(int: number) {
 }
 
 function stringToDate(text: string) {
+  if (!dateRegex.test(text)) return null;
+
   const parts = text.split('-');
 
   const date = new Date(2010, 0, 1);
@@ -102,4 +41,56 @@ function stringToDate(text: string) {
   date.setDate(parseInt(parts[2], 10));
 
   return date;
+}
+
+@Component({
+  selector: 'app-example-three',
+  templateUrl: './example-three.component.html',
+  styleUrls: ['./example-three.component.scss'],
+})
+export class ExampleThreeComponent implements OnInit {
+  inputControl = new FormControl({
+    value: '',
+    validators: dateValidatorFn,
+  });
+
+  dateControl = new FormControl<Date | null>({ value: null });
+
+  constructor() {}
+
+  ngOnInit() {
+    this.inputControl.changes
+      .pipe(
+        map(state => {
+          switch (state.type) {
+            case 'setValue':
+            case 'patchValue':
+              return {
+                ...state,
+                value: stringToDate(state.value),
+              };
+            default:
+              return state;
+          }
+        }),
+      )
+      .subscribe(this.dateControl.source);
+
+    this.dateControl.changes
+      .pipe(
+        map(state => {
+          switch (state.type) {
+            case 'setValue':
+            case 'patchValue':
+              return {
+                ...state,
+                value: dateToString(state.value),
+              };
+            default:
+              return state;
+          }
+        }),
+      )
+      .subscribe(this.inputControl.source);
+  }
 }
